3fbc65efd4fd49e8c2ce63b114dd9632f734df80
vwvuue <>
2023-06-09 12:15:35 +0300

    task10

---
+++ BotClient (task 10)/BotClient (task 10)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BotClient__task_10_
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
